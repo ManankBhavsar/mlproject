6a4c8574b81af085af00527caa5b342cf965a780
ManankBhavsar <ermanankbhavsar@gmail.com>
2023-05-24 17:21:31 +0200

    update exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-import logger
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
 
@@ -19,3 +19,4 @@ class CustomException(Exception):
     def __str__(self):
 
         return self.error_message
+        
